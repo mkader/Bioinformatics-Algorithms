deb8c0f3c632a5cdbd71eedcfa8f34a587abb69f
DayNightSoft <daynightsoft@gmail.com>
2019-04-14 11:21:09 -0500

    Create Shared k-mers.py

---
+++ Shared k-mers.py
@@ -0,0 +1,12 @@
+'''
+     Input: An integer k and two strings.
+            3
+            AAACTCATC
+            TTTCAAATC
+     Output: All k-mers shared by these strings, in the form of ordered pairs (x, y).
+             (0, 4)
+             (0, 0)
+             (4, 2)
+             (6, 6)
+
+'''
